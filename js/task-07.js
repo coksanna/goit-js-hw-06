e74c7d7fbcf973a2d41cc92961ed3509ae99530b
Oksana Kyryliuk <coksanna@gmail.com>
2022-11-05 14:06:00 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -4,6 +4,6 @@ const spanEl = document.querySelector('#text');
 // console.log(spanEl);
 inputEl.addEventListener('input', function(event) {
     event.preventDefault();
-    console.log(event.currentTarget.value);
+    // console.log(event.currentTarget.value);
     spanEl.style.fontSize = `${event.currentTarget.value}px`;
 })
